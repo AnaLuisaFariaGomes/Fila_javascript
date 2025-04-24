fb191c30bd00607b54724d4483465ebccfcff7e1
AnaLuisaFariaGomes <129781832+AnaLuisaFariaGomes@users.noreply.github.com>
2025-04-24 10:28:49 -0300

    Fila de atendimento (aula 24/04/2025)

---
+++ filaAtendimentoModelo2025/controllers/filaController.js
@@ -0,0 +1,30 @@
+const minhaFila = new Fila(5);
+
+function addElemento(){
+    const novoElemento = 
+       document.getElementById("txtnovoNome");
+    if(!minhaFila.isFull()){
+       minhaFila.enqueue(novoElemento.value);
+       mostrarFila();
+       novoElemento.value = " "; // limpa
+       novoElemento.focus(); // cursor no input
+    } 
+    else
+        alert("Fila cheia!");     
+} // fim add
+
+function mostrarFila() {
+    const listaFila = document.getElementById("listFila");
+    listaFila.textContent = minhaFila.toString();
+} // fim mostrar
+
+function atenderFila() {
+    if(!minhaFila.isEmpty()){
+        const atendido = minhaFila.dequeue();
+        alert("Pessoa atendida: " + atendido);
+        mostrarFila();
+        // salvar no banco de texto do navegador
+    } else {
+        alert("Fila vazia!!")
+    }
+}// fim atender
