efda55ac9c037f24e87a3082f327f1e6cf49326d
AnaLuisaFariaGomes <129781832+AnaLuisaFariaGomes@users.noreply.github.com>
2025-04-30 10:38:43 -0300

    Update 30/04 10:38

--- filaAtendimentoModelo2025/models/FilaCircular.js
+++ filaAtendimentoModelo2025/models/FilaCircular.js
@@ -1,6 +1,6 @@
-// FilaCircular.js
+// Fila circular
 
-class Fila {
+class FilaCircular {
     // # == private
     #inicio;
     #fim;
@@ -16,43 +16,63 @@ class Fila {
 
     // Métodos
     isFull() {
-    return this.#fim === this.#elementos.length -1;        
+    return this.#qtd === this.#elementos.length;        
     }
 
     isEmpty() {
-        return this.#fim < this.#inicio;
+        return this.#qtd === 0;
     }
 
-    enqueue(dado) {
-        if(!this.isFull()) {
-            this.#fim++;
+    enqueue(dado){
+        if(!this.isFull()){
+            if(this.#fim === this.#elementos.length-1)
+                this.#fim = 0;
+            else    
+                this.#fim++;
             this.#elementos[this.#fim] = dado;
             this.#qtd++;
+          console.log("Ini:"+this.#inicio+ 
+                 " Fim:"+this.#fim + " Qtd:"+ this.#qtd);
             return true;
-        }
-        return false; // A fila está cheia
+        }// fim if
+        else
+            return false; // se estiver cheio
     }
 
-    dequeue () {
-        if (!this.isEmpty()) {
-            const dado = this.#elementos[this.#inicio];
-            this.#inicio++;
+    dequeue(){
+        if(!this.isEmpty()){
+            const dado = 
+                 this.#elementos[this.#inicio];
+            if(this.#inicio === this.#elementos.length-1)
+                this.#inicio=0;
+            else
+                 this.#inicio++;
             this.#qtd--;
+          console.log("Ini:"+this.#inicio+ 
+                 " Fim:"+this.#fim + " Qtd:"+ this.#qtd);
             return dado;
-        }
-        else {
-            return null;
-        }
+        }// fim if
+        else
+            return null; // se estiver vazio 
     }
 
+    // VAI CAIR NA PROVA!!! ESTUDAR COMO E PQ FUNCIONA
     // toString deve ser criado em todas as classes que recebem dados
-    toString () {
-        let filaString = " ";
-        for (let i = this.#inicio; i <= this.#fim; i++) {
-            filaString += this.#elementos[i] + " | ";
-        }
+    toString(){
+        let filaString = "";
+        let pos = this.#inicio;
+        for(let i=0; i<this.#qtd;i++){
+            filaString += this.#elementos[pos] +" |";
+            if(pos === this.#elementos.length-1)
+                pos=0;
+            else
+                pos++;
+        }// fim for
         console.log(filaString);
         return filaString;
     }
+
+    // FAZER ITERATOR!!! O iterator possibilita percorrer o vetor posição por posição
+    // Symbol.iterator deve ser criado em todas as classes que recebem dados
 
 }// fim classe
